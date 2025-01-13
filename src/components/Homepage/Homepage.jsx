9dfaf61b1406851ab60ec3fa86c3a64cb29df4d5
Brandon Lin <brandonlin9716@gmail.com>
2025-01-12 21:43:50 -0800

    Extract and create site logo component

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,12 +1,12 @@
 import hero from '../../assets/hero.png';
+import { Logo } from '../logo/logo.jsx';
 import styles from './Homepage.module.css';
 
 const Homepage = () => {
   return (
     <div className={styles.layout}>
-      <nav>
-        <h1>Mech Industries</h1>
-        <button>Go</button>
+      <nav className={styles.nav}>
+        <Logo></Logo>
       </nav>
       <img src={hero} alt="Mecha schematics" className={styles.hero} />
     </div>
