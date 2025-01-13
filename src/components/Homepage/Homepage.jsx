42486a61524a860f8be874da07c4ffa8a27a16a3
Brandon Lin <brandonlin9716@gmail.com>
2025-01-13 13:29:30 -0800

    Make button grow when screen size grows

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
     <div className={styles.layout}>
       <nav className={styles.nav}>
         <img src={logo} alt="" className={styles.logo}/>
-        <button className={styles.button}>TAKE CONTROL</button>
+        <div className={styles.buttonHolder}><button className={styles.button}>TAKE CONTROL</button></div>
       </nav>
       <img src={hero} alt="Mecha schematics" className={styles.hero} />
     </div>
