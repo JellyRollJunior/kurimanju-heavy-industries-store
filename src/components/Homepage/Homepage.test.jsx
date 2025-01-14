dab7273187516351a96ec604c548cc2275843a8a
Brandon Lin <brandonlin9716@gmail.com>
2025-01-13 19:54:45 -0800

    Fix Homepage tests to work with Link element

--- src/components/Homepage/Homepage.test.jsx
+++ src/components/Homepage/Homepage.test.jsx
@@ -1,10 +1,15 @@
 import { describe, expect, it } from 'vitest';
 import { render } from '@testing-library/react';
 import { Homepage } from './Homepage';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Homepage component', () => {
   it('Renders Homepage with hero image, company logo, and nav button', () => {
-    const { container } = render(<Homepage />);
+    const { container } = render(
+      <MemoryRouter>
+        <Homepage />
+      </MemoryRouter>
+    );
     expect(container).toMatchSnapshot();
   });
 });
