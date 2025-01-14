5850dd60bfead06bd9532f16cba45245ccaedac9
Brandon Lin <brandonlin9716@gmail.com>
2025-01-13 20:31:19 -0800

    Create initial product card to display shop items

--- src/components/Storefront/Storefront.jsx
+++ src/components/Storefront/Storefront.jsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/logo.svg';
+import x4_base from '../../assets/x4_base.png';
 import styles from './Storefront.module.css';
 
 const Storefront = () => {
@@ -7,6 +8,17 @@ const Storefront = () => {
       <header className={styles.header}>
         <img src={logo} alt="" className={styles.logo} />
       </header>
+      <main className={styles.shop}>
+        <div className={styles.card}>
+          <img src={x4_base} alt="" />
+          <h3>X-4 Base</h3>
+          <h4>90 000 000</h4>
+          <form>
+            <input type="number" />
+            <button>Add to Cart</button>
+          </form>
+        </div>
+      </main>
     </div>
   );
 };
