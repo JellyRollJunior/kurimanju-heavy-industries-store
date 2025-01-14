3c97b0f1f08be0dd5645308a0cbe16b7204c6b24
Brandon Lin <brandonlin9716@gmail.com>
2025-01-14 13:58:19 -0800

    Add 3 products to shop list

--- src/data/productData.js
+++ src/data/productData.js
@@ -1,18 +1,39 @@
 import x4_base from '../assets/x4_base.png';
 import x4_stealth from '../assets/x4_stealth.png';
+import x4_respond from '../assets/x4_respond.png';
+import x4_enforce from '../assets/x4_enforce.png';
+import x4_supress from '../assets/x4_supress.png';
 
 const productData = [
     {
-        id: '1',
+        id: '0',
         name: 'X-4 Base',
         price: '90 000 000',
         img: x4_base,
     },
     {
-        id: '2',
+        id: '1',
         name: 'X-4 Stealth',
-        price: '91 000 000',
+        price: '95 000 000',
         img: x4_stealth,
+    },
+    {
+        id: '2',
+        name: 'X-4 Respond',
+        price: '95 000 000',
+        img: x4_respond,
+    },
+    {
+        id: '3',
+        name: 'X-4 Enforce',
+        price: '100 000 000',
+        img: x4_enforce,
+    },
+    {
+        id: '4',
+        name: 'X-4 Supress',
+        price: '120 000 000',
+        img: x4_supress,
     },
 ];
 
