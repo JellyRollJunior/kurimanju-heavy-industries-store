637b48b3825c84f1f78e7b82e774817ebf2ad54a
Brandon Lin <brandonlin9716@gmail.com>
2025-01-15 14:19:38 -0800

    Add items to cart by clicking "Add to cart" button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import styles from './Card.module.css';
 
-const Card = ({ name, price, img }) => {
+const Card = ({ id, name, price, img, onSubmit }) => {
+  const [quantity, setQuantity] = useState(0);
+  
+  const onSubmitCart = (event) => {
+    event.preventDefault();
+    onSubmit(id, quantity);
+  }
+
   return (
     <div className={styles.card}>
       <img src={img} alt="" />
       <h3>{name}</h3>
       <h4>{price}</h4>
-      <form>
-        <input type="number" />
+      <form onSubmit={(event) => onSubmitCart(event)}>
+        <input type="number" value={quantity} onChange={(event) => setQuantity(event.target.value)} required />
         <button>Add to Cart</button>
       </form>
     </div>
--- src/components/Storefront/Storefront.jsx
+++ src/components/Storefront/Storefront.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import logo from '../../assets/logo.svg';
 import styles from './Storefront.module.css';
 import { productData } from '../../data/productData.js';
 import { Card } from '../Card/Card.jsx';
 
 const Storefront = () => {
+  const [cart, setCart] = useState(Array(productData.length).fill(0));
+
+  const addToCart = (id, quantity) => {
+    const newCart = [...cart];
+    newCart[id] += Number(quantity);
+    setCart(newCart);
+  };
+
   return (
     <div className={styles.layout}>
       <header className={styles.header}>
@@ -13,9 +22,11 @@ const Storefront = () => {
         {productData.map((item) => (
           <Card
             key={item.id}
+            id={item.id}
             name={item.name}
             price={item.price}
             img={item.img}
+            onSubmit={addToCart}
           ></Card>
         ))}
       </main>
