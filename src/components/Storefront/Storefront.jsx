db4599bac539fa7576c10afd046e533e7955e943
Brandon Lin <brandonlin9716@gmail.com>
2025-01-17 10:42:57 -0800

    Extract header into component

--- src/components/Storefront/Storefront.jsx
+++ src/components/Storefront/Storefront.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react';
-import logo from '../../assets/logo.svg';
-import headerIcon from '../../assets/header-icon.png';
-import headerPortrait from '../../assets/header-portrait.png';
-import headerHudText from '../../assets/header-hud-text.png';
 import styles from './Storefront.module.css';
 import { productData } from '../../data/productData.js';
+import { Header } from '../Header/Header.jsx';
 import { Card } from '../Card/Card.jsx';
 
 const Storefront = () => {
@@ -18,29 +15,7 @@ const Storefront = () => {
 
   return (
     <div className={styles.layout}>
-      <header className={styles.header}>
-        <img src={headerIcon} alt="" className={styles.headerIcon} />
-        <img src={logo} alt="" className={styles.logo} />
-        <div className={styles.headerDecoration}>
-          <p className={styles.chineseText}>
-            機体究更
-            <br />
-            武裝解除
-            <br />
-            決定中止
-          </p>
-          <p className={styles.englishText}>
-            F91 EARTH-WIND RISES
-            <br />
-            LIBERATION X-97
-            <br />
-            DE- 524 - ITY
-          </p>
-          <div></div>
-          <img src={headerPortrait} alt="" className={styles.portrait} />
-        </div>
-        <img src={headerHudText} alt="" className={styles.hudText} />
-      </header>
+      <Header />
       <main className={styles.shop}>
         {productData.map((item) => (
           <Card
