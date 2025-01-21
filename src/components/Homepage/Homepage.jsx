0c58e572db98272108fb1cbf63e182243ff14818
Brandon Lin <brandonlin9716@gmail.com>
2025-01-20 17:58:52 -0800

    Add icon and borders to mobile view homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,18 +1,24 @@
-import hero from '../../assets/hero.png';
+import headerIcon from '../../assets/header-icon.png';
 import logo from '../../assets/logo.svg';
+import hero from '../../assets/hero.png';
 import styles from './Homepage.module.css';
 import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   return (
-    <div className={styles.layout}>
-      <nav className={styles.nav}>
-        <img src={logo} alt="" className={styles.logo} />
-        <div className={styles.buttonHolder}>
-          <Link to="storefront" className={styles.navButton}>TAKE CONTROL</Link>
-        </div>
-      </nav>
-      <img src={hero} alt="Mecha schematics" className={styles.hero} />
+    <div className={styles.wrapper}>
+      <div className={styles.layout}>
+        <nav className={styles.nav}>
+          <img src={headerIcon} alt="" className={styles.headerIcon} />
+          <img src={logo} alt="" className={styles.logo} />
+          <div className={styles.buttonHolder}>
+            <Link to="storefront" className={styles.navButton}>
+              TAKE CONTROL
+            </Link>
+          </div>
+        </nav>
+        <img src={hero} alt="Mecha schematics" className={styles.hero} />
+      </div>
     </div>
   );
 };
