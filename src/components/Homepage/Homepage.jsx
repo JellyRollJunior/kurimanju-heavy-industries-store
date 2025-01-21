37d56221a87087cb1bf19fa315dd6200e4c79d2a
Brandon Lin <brandonlin9716@gmail.com>
2025-01-20 18:10:21 -0800

    Add HUD text image and remove unnecessary HTMl from homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,24 +1,22 @@
 import headerIcon from '../../assets/header-icon.png';
 import logo from '../../assets/logo.svg';
 import hero from '../../assets/hero.png';
+import hudText from '../../assets/header-hud-text.png';
 import styles from './Homepage.module.css';
 import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.layout}>
-        <nav className={styles.nav}>
-          <img src={headerIcon} alt="" className={styles.headerIcon} />
-          <img src={logo} alt="" className={styles.logo} />
-          <div className={styles.buttonHolder}>
-            <Link to="storefront" className={styles.navButton}>
-              TAKE CONTROL
-            </Link>
-          </div>
-        </nav>
-        <img src={hero} alt="Mecha schematics" className={styles.hero} />
-      </div>
+    <div className={styles.layout}>
+      <nav className={styles.nav}>
+        <img src={headerIcon} alt="" className={styles.headerIcon} />
+        <img src={logo} alt="" className={styles.logo} />
+        <Link to="storefront" className={styles.navButton}>
+          TAKE CONTROL
+        </Link>
+      </nav>
+      <img src={hero} alt="Mecha schematics" className={styles.hero} />
+      <img src={hudText} alt="HUD text" className={styles.hudText} />
     </div>
   );
 };
