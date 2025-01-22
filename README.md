8fad6907dc115e5f4745d3d9eee41b000f1b6c44
Brandon Lin <brandonlin9716@gmail.com>
2025-01-22 12:48:13 -0800

    Remove form from Card component. Add proptypes to Card
    Clicking increment button was submitting the form. submitting the form
    will add items to the cart.

--- README.md
+++ README.md
@@ -7,8 +7,6 @@
 ### todos:
 - perhaps extract cart login into js file for more complete testing? because testing react state is not convenient
 - footer
-- Bug: clicking increment button adds item to cart
-
 
 ### Features
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -13,38 +13,46 @@ const Card = ({
 }) => {
   const [quantity, setQuantity] = useState(0);
 
-  const onSubmitCart = (event) => {
-    event.preventDefault();
-    onSubmit(id, quantity);
-  };
-
   const decrementQuantity = () => {
     setQuantity(Math.max(0, quantity - 1));
-  }
+  };
 
   const incrementQuantity = () => {
     setQuantity(Math.min(maxQuantityPerItem, quantity + 1));
-  }
+  };
 
   return (
     <div className={styles.card}>
       <img src={img} alt={name} />
-      <form onSubmit={(event) => onSubmitCart(event)} className={styles.form}>
-        <h3 className={styles.itemName}>{name}</h3>
-        <h4>{price}</h4>
-        <div className={styles.quantityHolder}>
-          <button onClick={decrementQuantity} className={`${styles.quantityButton} ${styles.decrementButton}`}>-</button>
-          <input
-            type="number"
-            value={quantity}
-            required
-            disabled
-            className={styles.quantity}
-          />
-          <button onClick={incrementQuantity} className={`${styles.quantityButton} ${styles.incrementButton}`}>+</button>
-        </div>
-        <button className={styles.submitBtn}>Add to Cart</button>
-      </form>
+      <h3 className={styles.itemName}>{name}</h3>
+      <h4 className={styles.price}>{price}</h4>
+      <div className={styles.quantityHolder}>
+        <button
+          onClick={decrementQuantity}
+          className={`${styles.quantityButton} ${styles.decrementButton}`}
+        >
+          -
+        </button>
+        <input
+          type="number"
+          value={quantity}
+          required
+          disabled
+          className={styles.quantity}
+        />
+        <button
+          onClick={incrementQuantity}
+          className={`${styles.quantityButton} ${styles.incrementButton}`}
+        >
+          +
+        </button>
+      </div>
+      <button
+        onClick={() => onSubmit(id, quantity)}
+        className={styles.submitBtn}
+      >
+        Add to Cart
+      </button>
     </div>
   );
 };
@@ -54,6 +62,7 @@ Card.propTypes = {
   name: PropTypes.string,
   price: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   img: PropTypes.string,
+  maxQuantityPerItem: PropTypes.number,
   onSubmit: PropTypes.func.isRequired,
 };
 
