938ba705cad92e25db8437a8d72157b1516570ab
Brandon Lin <brandonlin9716@gmail.com>
2025-01-23 13:51:32 -0800

    Remove module from non module CSS files

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { routes } from './routes.jsx'
-import './styles/reset.module.css';
-import './styles/global.module.css';
+import { routes } from './routes.jsx';
+import './styles/reset.css';
+import './styles/global.css';
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
